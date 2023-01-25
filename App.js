97fc61f2505695fbe19a87f4d247c15072b0a5b3
LeonardRingot <leonardringot6@gmail.com>
2023-01-25 21:25:21 +0100

    [WIP] - search

--- App.js
+++ App.js
@@ -1,34 +1,85 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet,SafeAreaView, Text, View , TextInput, Button, TouchableOpacity, Pressable} from 'react-native';
+import React,{useState,useEffect} from 'react';
 
+import { StyleSheet,SafeAreaView, Text, View , TextInput, Button,FormControl, TouchableOpacity, Pressable, Form} from 'react-native';
 
-export default function App()  {
-  const [search, setsearch] = React.useState('Useless Text');
+const API_URL="https://api.themoviedb.org/3/movie/popular?api_key=dbd1168e4b7b2f9cca7c17bc58925157";
+const API_SEARCH="https://api.themoviedb.org/3/search/movie?api_key=dbd1168e4b7b2f9cca7c17bc58925157&query";
+
+ function App()  {
+  const [movies, setMovies]=useState([]);
+  const [query, setQuery]=useState('');
+  useEffect(() => {
+    fetch(API_URL)
+    .then((res)=>res.json())
+    .then(data=>{
+      console.log(data);
+      setMovies(data.results);
+    })
+  }, [])
+  const searchMovie = async(e)=>{
+    e.preventDefault();
+    console.log("Searching");
+    try{
+      const url=`https://api.themoviedb.org/3/search/movie?api_key=dbd1168e4b7b2f9cca7c17bc58925157&query=${query}`;
+      const res= await fetch(url);
+      const data= await res.json();
+      console.log(data);
+      setMovies(data.results);
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+  const changeHandler=(e)=>{
+    setQuery(e.target.value);
+  }
   return (
+    <>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! TEST COMMIT</Text>
-      <StatusBar style="auto" />
+      {/* <StatusBar style="auto" />
       <SafeAreaView>
-
-     
       <TextInput
             style={styles.input}
             placeholder='Rechercher un film'
-            onChangeText={setsearch}
-        value={search}
+          
           />
            </SafeAreaView>
-          <Pressable style={styles.btn} onPress={() => Alert.alert('Left button pressed')} ><Text style={styles.txtbtn}>Rechercher</Text></Pressable>
+          <Pressable style={styles.btn} onPress={() => Alert.alert('Left button pressed')} ><Text style={styles.txtbtn}>Rechercher</Text></Pressable> */}
+          <Form className="d-flex" onSubmit={searchMovie} autoComplete="off">
+              <FormControl
+              type="search"
+              placeholder="Movie Search"
+              className="me-2"
+              aria-label="search"
+              name="query"
+              value={query} onChange={changeHandler}></FormControl>
+              <Button variant="secondary" type="submit">Search</Button>
+            </Form>
+            
     </View>
+    <div>
+      {movies.length > 0 ?(
+        <div className="container">
+        <div className="grid">
+          {movies.map((movieReq)=>
+          <MovieBox key={movieReq.id} {...movieReq}/>)}
+            </div>
+    </div>
+      ):(
+        <Text>Sorry !! No Movies Found</Text>
+      )}
+    </div>   
+    </>
   );
 }
 
-//https://api.themoviedb.org/3/movie/550?api_key=dbd1168e4b7b2f9cca7c17bc58925157
+export default App;
 
 //dbd1168e4b7b2f9cca7c17bc58925157
 
-//eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYmQxMTY4ZTRiN2IyZjljY2E3YzE3YmM1ODkyNTE1NyIsInN1YiI6IjYzZDE2ODE5OWU0NTg2MDBiMjdjOGYzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H-_98Rr6pz2NOjr7nvbWI80fFHZ609UoEeb6dFk7dHs
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
